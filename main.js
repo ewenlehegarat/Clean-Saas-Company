06f4a23cc2de511bd43745aa875c46884e6489f2
Ewen Le Hegarat <ewenlehegaratpro@gmail.com>
2026-01-21 21:00:25 +0100

    update

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const observerNav = new IntersectionObserver((entries) => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("nav_view");
             }
-        }, "500");
+        }, 500);
     });
 }, {});
 observerNav.observe(nav);
@@ -29,7 +29,7 @@ const observerHero = new IntersectionObserver((entries) => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("hero_view");
             }
-        }, "200");
+        }, 200);
     });
 }, {});
 observerHero.observe(heroImage);
@@ -41,7 +41,7 @@ const observerEvery = new IntersectionObserver((entries) => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("every_view");
             }
-        }, "300");
+        }, 300);
     });
 }, {});
 observerEvery.observe(everythingText);
@@ -53,10 +53,20 @@ const observerContent = new IntersectionObserver((entries) => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("content_view");
             }
-        }, "600");
+        }, 600);
     });
 }, {});
 observerContent.observe(featuresContent);
+
+const valueHeader = document.querySelector('.value_section .header')
+const observerHeader = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("header_view");
+        }
+    });
+}, {});
+observerHeader.observe(valueHeader);
 
 const valueModules = document.querySelector('.value_modules')
 const observerValue = new IntersectionObserver((entries) => {
@@ -65,10 +75,54 @@ const observerValue = new IntersectionObserver((entries) => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("value_view");
             }
-        }, "600");
+        }, 600);
     });
 }, {});
 observerValue.observe(valueModules);
+
+const valueCase = document.querySelector('.case_study_module')
+const observerCase = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("case_view");
+        }
+    });
+}, {});
+observerCase.observe(valueCase);
+
+const valueBlog = document.querySelector('.blog_section')
+const observerBlog = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("blog_view");
+        }
+    });
+}, {});
+observerBlog.observe(valueBlog);
+
+const testimonial = document.querySelector('.testimonial_section .content img')
+const observerTesti = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("testi_view");
+        }
+    });
+}, {});
+observerTesti.observe(testimonial);
+
+const testiQuote = document.querySelector('.quote');
+const observerQuote = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            setTimeout(() => {
+                entry.target.classList.add("quote_view");
+            }, 600);
+        }
+    });
+});
+observerQuote.observe(testiQuote);
+
+
 
 if(window.innerWidth < 800){
     const menuIcon = document.querySelector('.menu_icon');
